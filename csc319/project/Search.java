09960fcffc2e94853ec26d1761393dd8c6bbb81c
NotBattleFrog <tigergood@live.com>
2018-04-07 22:47:18 +0700

    Add Main and Search file

---
+++ csc319/project/Search.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package csc319.project;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Tiger
+ */
+public class Search {
+    private int totalCount;
+    private ArrayList<String> sentence;
+
+    
+    public int getTotalcount() {
+        return totalCount;
+    }
+
+    public ArrayList<String> getSentence() {
+        return sentence;
+    }
+    
+    private void beginSearch(String keyWord){
+        
+    }
+    
+    private void putKeyword(){
+        
+    }
+    
+    private void readTextFile(){
+        
+    }
+    
+    private void printAllSentence(){
+        
+    }
+    
+    
+    
+    
+}
