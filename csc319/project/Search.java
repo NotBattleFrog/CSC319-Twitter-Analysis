d2901e8a3face2561ac029909886311c9fd05a7f
NotBattleFrog <tigergood@live.com>
2018-05-05 12:43:01 +0700

    Combine and improve the 2 searches
    2 searches ( Text and Twitter4J )

--- csc319/project/Search.java
+++ csc319/project/Search.java
@@ -7,55 +7,124 @@ package csc319.project;
 
 import java.io.*;
 import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Scanner;
+import twitter4j.Query;
+import twitter4j.QueryResult;
+import twitter4j.SavedSearch;
+import twitter4j.Status;
+import twitter4j.Twitter;
+import twitter4j.TwitterException;
+import twitter4j.TwitterFactory;
+import twitter4j.conf.ConfigurationBuilder;
 
-
+/**
+ *
+ * @author Tiger
+ */
 public class Search {
+    private String keyword;
     private int totalCount;
-    private ArrayList<String> sentence;
+    //private String consumerkey, consumersecret, accesstoken, accesstokensecret ;
+    //private Configuration con;
+    private ArrayList<String> searched;
 
+    public Search() {
+        this(null);
+    }
+    
+    public Search(String keyword) {
+        this.keyword = keyword;
+        searched = new ArrayList<String>();
+    }
+    
+   /* public void buildAcc(String consumerkey, String consumersecret, String accesstoken, String accesstokensecret) {
+        con = new Configuration(consumerkey, consumersecret, accesstoken, accesstokensecret);
+    }*/
+
+    public String getKeyword() {
+        return keyword;
+    }
+
+    public void setKeyword(String keyword) {
+        this.keyword = keyword;
+    }
+    
     
     public int getTotalcount() {
         return totalCount;
     }
-
-    public ArrayList<String> getSentence() {
-        return sentence;
-    }
     
-    private void searchKeyWord(String keyWord){
-        
-    }
-    
-    private void putKeyword(){
-        
-    }
-    
-    private void readTextFile(){
-        String FileName = "Input.txt";
-        String line = null;
+    public void textSearch(File file) throws FileNotFoundException{
+        int count =0;
+        String line;
         try {
-            FileReader fileReader = new FileReader("Input.txt");
-            BufferedReader bufferedReader = new BufferedReader(fileReader);
-            
+            BufferedReader bufferedReader = new BufferedReader(new FileReader(file));
             while((line = bufferedReader.readLine()) != null) {
-                System.out.println(line);
+            if(line.contains(keyword)){
+                count++;
+                System.out.println(count+". "+line);
+                searched.add(line);
+            }
             }   
-
+            totalCount =0;
             bufferedReader.close();         
         }
-        catch(FileNotFoundException ex) {
-            System.out.println(  "Unable to open file " + "Input.txt" );                
-        }
         catch(IOException ex) {
-            System.out.println(  "Error reading file " + "input.txt" );     
+            System.out.println(
+                "Error reading file '" 
+                + "input.txt" + "'");     
         }
     }
     
-    private void printAllSentence(){
+
+      public void searchtw4j(){
+        ConfigurationBuilder cb = new ConfigurationBuilder();
+        cb.setDebugEnabled(true).setOAuthConsumerKey("").setOAuthConsumerSecret("")
+                                .setOAuthAccessToken("").setOAuthAccessTokenSecret("");
         
+        TwitterFactory tf = new TwitterFactory(cb.build());
+        twitter4j.Twitter twitter =  tf.getInstance();
+        
+           int count =0;
+             try {
+                 String sentence;
+            Query query = new Query(keyword);
+            QueryResult result;
+            do {
+               result = twitter.search(query);
+                List<Status> tweets = result.getTweets();
+                  for (Status tweet : tweets) { 
+                      
+               searched.add(". User : "+ tweet.getUser().getScreenName()+ " ---- " + tweet.getText());
+                count++;
+                }
+            } while ((query = result.nextQuery()) != null );
+        
+        } catch (TwitterException e) {
+        }
+             totalCount = count;
     }
+
+      
+    public void printAllSentence(){
+        int count =0;
+           Iterator it = searched.iterator();
+             while(it.hasNext()){
+         count++;
+         System.out.println(count+"."+it.next());
+     }
+
+    }
+
+        
+
+   
     
+    }
+        
+      
     
-    
-    
-}
+
+
